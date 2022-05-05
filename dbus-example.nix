61cb887c2c1e14c62f08ac22e2fd3b58f6e613b8
Jesse <jesseholzman96@gmail.com>
2022-05-05 14:04:38 -0400

    Add extra dependencies

--- dbus-example.nix
+++ dbus-example.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, dbus, lib }:
+{ mkDerivation, base, containers, dbus, directory, lib }:
 mkDerivation {
   pname = "dbus-example";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base dbus ];
+  executableHaskellDepends = [ base containers dbus directory ];
   license = "unknown";
   hydraPlatforms = lib.platforms.none;
 }
